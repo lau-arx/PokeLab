88c22f395c9c8579a95842afec2879707656b3ad
Zelal Aslan <zelalaslan2@gmail.com>
2021-05-18 10:46:18 +0200

    we can not finish the controller without  pundit

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -13,13 +13,25 @@ class PokemonsController < ApplicationController
 
   def create
     @pokemon = Pokemon.new(pokemon_params)
-    @pokemon.save
-    redirect_to pokemons_path
+    @pokemon.user = current_user
+    if @pokemon.save
+      redirect_to pokemons_path
+    else
+      render :new
+    end
   end
+
+  # def edit
+  #   @pokemon = Pokemon.find(params[:id_edit])
+  # end
+
+  # def update
+
+  # end
 
   private
 
   def pokemon_params
-    params.require(:pokemon).permit(:name)
+    params.require(:pokemon).permit(:user, :name, :category, :description, :height, :weight, :special_attack, :price_per_day, :health_point)
   end
 end
