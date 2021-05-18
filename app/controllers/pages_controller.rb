121b99f2881b95973cd396b2f99def471414483d
Laura Auroux <laura.auroux@live.fr>
2021-05-18 18:56:25 +0200

    WIP - tabs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @pokemons = Pokemon.where(user: current_user)
+    @bookings = Booking.where(user: current_user)
+  end
 end
---
+++ app/javascript/components/tab.js
@@ -0,0 +1,5 @@
+const changeTabs = () => {
+  const selectedTab = document.querySelector(".tab");
+  console.log(selectedTab);
+}
+export { changeTabs };
