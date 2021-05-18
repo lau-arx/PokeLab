ee0e9affc969f633db8a1bd0a92c893e0307c526
Laura Auroux <laura.auroux@live.fr>
2021-05-18 23:21:27 +0200

    tabs functioning

--- app/javascript/components/tab.js
+++ app/javascript/components/tab.js
@@ -1,5 +1,41 @@
+const selectedContents = document.querySelectorAll(".content");
+
+console.log(selectedContents)
+
+const selectedTabs = document.querySelectorAll(".tab");
+
 const changeTabs = () => {
-  const selectedTab = document.querySelector(".tab");
-  console.log(selectedTab);
+  selectedTabs.forEach((tab) => {
+    tab.addEventListener('click', (event) => {
+      if (event.currentTarget.classList.contains('tab-active') === false) {
+        hideActive()
+        contentHidden()
+        event.currentTarget.classList.add('tab-active');
+        // console.log(event.currentTarget.attributes.id.value[4])
+
+        selectedContents.forEach((content) => {
+          // console.log(content.attributes.id.value[8])
+          if (content.attributes.id.value[8] === event.currentTarget.attributes.id.value[4]) {
+            content.classList.add('displayed');
+            content.classList.remove('hidden');
+          };
+        });
+      };
+    });
+  });
+};
+
+const hideActive = () => {
+  selectedTabs.forEach((tab) => {
+    tab.classList.remove('tab-active');
+  })
 }
+
+const contentHidden = () => {
+  selectedContents.forEach((content) => {
+    content.classList.remove('displayed');
+    content.classList.add('hidden');
+  })
+}
+
 export { changeTabs };
