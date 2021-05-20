f3e77da8773fb74a0e2f361744dc7316f8b638db
Luc de Courcel <lucdecourcel@icloud.com>
2021-05-20 17:30:01 +0200

    booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-sacha = User.create!(email: "sacha@gmail.com", password: "password")
-Pokemon.create!(user: sacha, name: "pikachu", category: "electricite", description: "blabla", height: 50, weight: 3, special_attack: "foudre", price_per_day: 15, health_point: 50)
+# sacha = User.create!(email: "sacha@gmail.com", password: "password")
+# Pokemon.create!(user: sacha, name: "pikachu", category: "electricite", description: "blabla", height: 50, weight: 3, special_attack: "foudre", price_per_day: 15, health_point: 50)
