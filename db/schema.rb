9fccca1fbdfebb4842cd6ac6cedb53c8cb54db73
Luc de Courcel <lucdecourcel@icloud.com>
2021-05-20 10:59:37 +0200

    trash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_070814) do
+ActiveRecord::Schema.define(version: 2021_05_20_082710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_070814) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.boolean "available", default: true
     t.index ["user_id"], name: "index_pokemons_on_user_id"
   end
 
