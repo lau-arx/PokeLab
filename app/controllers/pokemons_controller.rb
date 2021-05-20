4ccf4fa69cef38db3ce5eda355859b5d5fe8cf42
Zelal Aslan <zelalaslan2@gmail.com>
2021-05-20 18:35:07 +0200

    not really usefull work haha

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,62 +1,62 @@
-class PokemonsController < ApplicationController
-  before_action :find_pokemon, only: [:show, :edit, :update, :destroy]
-  def index
-    @pokemons = policy_scope(Pokemon).order(created_at: :desc)
+  class PokemonsController < ApplicationController
+    before_action :find_pokemon, only: [:show, :edit, :update, :destroy]
+    def index
+      @pokemons = policy_scope(Pokemon).order(created_at: :desc)
 
-    @pokemons = @pokemons.where("name ILIKE ?", "%#{params[:pokemon_name]}%") if params[:pokemon_name]
-    @pokemons = @pokemons.where("address ILIKE ?", "%#{params[:location]}%") if params[:location]
-    @pokemons = @pokemons.where("category ILIKE ?", "%#{params[:category]}%") if params[:category]
+      @pokemons = @pokemons.where("name ILIKE ?", "%#{params[:pokemon_name]}%") if params[:pokemon_name]
+      @pokemons = @pokemons.where("address ILIKE ?", "%#{params[:location]}%") if params[:location]
+      @pokemons = @pokemons.where("category ILIKE ?", "%#{params[:category]}%") if params[:category]
 
+      # @pokemons = @pokemons.near(params[:search], 25)
 
-    # @pokemons = @pokemons.near(params[:search], 25)
-    @markers = @pokemons.geocoded.map do |pokemon|
-      {
-        lat: pokemon.latitude,
-        lng: pokemon.longitude
-      }
+      @markers = @pokemons.geocoded.map do |pokemon|
+        {
+          lat: pokemon.latitude,
+          lng: pokemon.longitude
+        }
+      end
     end
-  end
 
-  def show
-  end
+    def show
+    end
 
-  def new
-    @pokemon = Pokemon.new
-    authorize @pokemon
-  end
+    def new
+      @pokemon = Pokemon.new
+      authorize @pokemon
+    end
 
-  def create
-    @pokemon = Pokemon.new(pokemon_params)
-    authorize @pokemon
-    @pokemon.user = current_user
-    if @pokemon.save
+    def create
+      @pokemon = Pokemon.new(pokemon_params)
+      authorize @pokemon
+      @pokemon.user = current_user
+      if @pokemon.save
+        redirect_to pokemons_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      @pokemon.update(pokemon_params)
+      redirect_to pokemon_path
+    end
+
+    def destroy
+      @pokemon.destroy
       redirect_to dashboard_path
-    else
-      render :new
+    end
+
+    private
+
+    def pokemon_params
+      params.require(:pokemon).permit(:photo, :user, :name, :category, :description, :height, :weight, :special_attack, :price_per_day, :health_point, :address)
+    end
+
+    def find_pokemon
+      @pokemon = Pokemon.find(params[:id])
+      authorize @pokemon
     end
   end
-
-  def edit
-  end
-
-  def update
-    @pokemon.update(pokemon_params)
-    redirect_to pokemon_path
-  end
-
-  def destroy
-    @pokemon.destroy
-    redirect_to dashboard_path
-  end
-
-  private
-
-  def pokemon_params
-    params.require(:pokemon).permit(:photo, :user, :name, :category, :description, :height, :weight, :special_attack, :price_per_day, :health_point, :address)
-  end
-
-  def find_pokemon
-    @pokemon = Pokemon.find(params[:id])
-    authorize @pokemon
-  end
-end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -10,4 +10,31 @@ class Pokemon < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
 end
+
+  # def period_available(start, end)
+  #     all_booking = bookings.where("start_date < :start AND end_date > :start", start: start)
+  #                           .or(bookings.where("start_date < :start AND end_date > :end", start: start, end: end))
+  #                           .or(bookings.where("start_date < :start AND end_date > :end"))
+  #     all_booking.empty?
+  # end
+
+  # def availability(start, end)
+
+  # Recuperer tous les booking qui sont entre la start et end
+  # Ou a cheval sur la periode start a end
+
+  # construire un array qui contient tous les jours de la start a la end
+
+  # Iterer sur tous les booking qu'on a recupéré
+    # Iterer sur tous les jours pris par les booking
+    # On supprime ce jour dans notre array
+  #end
+
+  # Si l'array est vite je retourne false si il n'est pas vide je retoune true
+
+
+
+
+
