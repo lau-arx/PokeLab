af4258a36b95317444a02180b856378bf8af91c5
Laura Auroux <laura.auroux@live.fr>
2021-05-21 12:28:00 +0200

    reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @pokemon = @booking.pokemon
+    authorize @booking
+    authorize @review
+    authorize @pokemon
+    @review.booking = @booking
+
+    if @review.save 
+      redirect_to dashboard_path(tab: 'content-2')
+    else
+      render 'pages/dashboard'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :booking_id)
+  end
+end
