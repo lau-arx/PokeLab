ea6ee07949c2412abf54b8bfd37debc94323faff
Zelal Aslan <zelalaslan2@gmail.com>
2021-05-21 10:16:41 +0200

    clikable button hom ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,30 @@ class PagesController < ApplicationController
     @pokemons = Pokemon.where(user: current_user)
     @bookings = Booking.where(user: current_user)
   end
+
+  def random_shot
+    @pokemons = Pokemon.all
+    # @pokemons = policy_scope(Pokemon).order(created_at: :desc)
+    # @pokemons = Pokemon.sample
+  end
+
+  def know_more
+
+  end
+
+  def heavy_stuffs
+
+  end
+
+  def true_freedom
+
+  end
+
+  def hazardous_places
+
+  end
+
+  def cozy_day
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
+  get 'random_shot', to:'pages#random_shot'
+  get 'know_more', to: 'pages#know_more'
+  get 'heavy_stuffs', to: 'pages#heavy_stuffs'
+  get 'true_freedom', to: 'pages#true_freedom'
+  get 'hazardous_places', to: 'pages#hazardous_places'
+  get 'cozy_day', to: 'pages#cozy_day'
+
   resources :pokemons do
     resources :bookings, only: [ :new, :create, :index, :edit, :update]
   end
